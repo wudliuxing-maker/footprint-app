8fe78fb1746bd2ccf8e1a243de62e275f27440f0
wudliuxing-maker <wudliuxing@gmail.com>
2026-03-06 14:36:45 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.footprint.app',
+  appName: '地图足迹',
+  webDir: 'dist'
+};
+
+export default config;
